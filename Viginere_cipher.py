ce2159075bc4b7e5aeb3c37eb6046fae2b05daea
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-18 21:16:25 +0800

    creating dictionaries to convert text to num and vice versa

--- Viginere_cipher.py
+++ Viginere_cipher.py
@@ -3,7 +3,11 @@
 #Creating a Vigenere Cipher program 
 
 #create a dictionary translating each character to their corresponding numerical value, and vice versa
+TEXT_TO_NUM = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,'L':11,'M':12,'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,'Z':25}
+NUM_TO_TEXT = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
+
 #create a function that has the message and key as its parameters used for encryption
+
     #convert each character in the message and key into their corresponding numerical values
     #convert each character in the message to numbers from 0-25 according to the dictionary
     #convert each character in the key to numbers from 0-25
