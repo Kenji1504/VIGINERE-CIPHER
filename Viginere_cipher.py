e010da85026063a96917e94061bd741d820e8b06
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-18 21:07:57 +0800

    Pseudocode

---
+++ Viginere_cipher.py
@@ -0,0 +1,24 @@
+#Ken Leam G. Gamboa
+#BSCPE 1-5
+#Creating a Vigenere Cipher program 
+
+#create a dictionary translating each character to their corresponding numerical value, and vice versa
+#create a function that has the message and key as its parameters used for encryption
+    #convert each character in the message and key into their corresponding numerical values
+    #convert each character in the message to numbers from 0-25 according to the dictionary
+    #convert each character in the key to numbers from 0-25
+    #add both values, then mod 26
+    #revert the numerical value back into text
+    #append new characters into ciphertext
+    #proceed to the next character
+    #in case there are non-alphabet caharacters, append to the ciphertext
+
+#create title design and introduction of the program
+#ask for user's input
+#call out the function
+#print the ciphertext
+#ask user if they want try again
+    #if yes, repeat the program
+    #if no, break
+    #else, prompt invalid input then ask user to try again
+#create program ender
